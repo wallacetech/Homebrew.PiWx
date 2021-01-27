64342a2da328b820d8bd82689c18e34f7b344c40
Jason <jason@wallacetech.com>
2021-01-26 20:53:22 -0800

    Initial Commit

---
+++ sdr.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+cmd = 'rtl_433 -M utc -F json'
+
+process = os.popen(cmd)
+
+while(True)
+    str = process.read()
+    print(str)
+    print("=======================")
+
+process.close()
