dda06835884c655f31fe2e990cf4197807859e4f
Jason <Jason@Wallacetech.com>
2021-02-03 21:35:09 -0800

    Add asynchronousfilereaer class

--- sdr.py
+++ sdr.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import secrets
 from subprocess import Popen, PIPE
-
-DEFAULT_COMMAND = "rtl_433 -M utc -F json"
-
+print(secrets.IO_CONNECTION_STRING)
+DEFAULT_COMMAND = "rtl_433 -M utc -R 40 -C si -F json"
+"""
 
 print("Opening process")
 process = Popen(DEFAULT_COMMAND.split(' '),stdout=PIPE,stderr=PIPE)
@@ -18,3 +19,4 @@ while True:
     print(line)
 
 process.close()
+"""
